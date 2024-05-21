ce87a6a824396e4117e0cb3c57366fe7f43d1d82
Jakob Watson <jakob@ianwatson.org>
2024-05-21 16:47:31 +0200

    initial working copy

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo -n "Removing existing theme..."
+sudo rm -r /usr/share/plymouth/themes/shrek/
+echo " Done"
+echo -n "Copying new theme..."
+sudo rsync -av --exclude ".git" . /usr/share/plymouth/themes/shrek/
+echo " Done"
+echo -n "Setting new default theme (this might take a while)..."
+sudo plymouth-set-default-theme -R shrek
+echo " Done"
---
+++ viewvm.sh
@@ -0,0 +1,8 @@
+echo -n "Removing existing theme..."
+rm -r ~/VM\ Shared/shrek/
+echo " Done"
+echo -n "Copying new theme..."
+rsync -av --exclude ".git" . ~/VM\ Shared/shrek/
+echo " Done"
+touch ~/VM\ Shared/touch.txt
+echo "Updated touch file"
