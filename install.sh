02b8da5614a08a824be7bfc8cf199dd4c678ba67
Jakob Watson <jakob@ianwatson.org>
2024-05-21 17:19:40 +0200

    the shreks appear! also added the vm files to the repo and made rsync not verboose.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo -n "Removing existing theme..."
 sudo rm -r /usr/share/plymouth/themes/shrek/
 echo " Done"
 echo -n "Copying new theme..."
-sudo rsync -av --exclude ".git" . /usr/share/plymouth/themes/shrek/
+sudo rsync -a --exclude ".git" . /usr/share/plymouth/themes/shrek/
 echo " Done"
 echo -n "Setting new default theme (this might take a while)..."
 sudo plymouth-set-default-theme -R shrek
--- viewvm.sh
+++ viewvm.sh
@@ -2,7 +2,7 @@ echo -n "Removing existing theme..."
 rm -r ~/VM\ Shared/shrek/
 echo " Done"
 echo -n "Copying new theme..."
-rsync -av --exclude ".git" . ~/VM\ Shared/shrek/
+rsync -a --exclude ".git" . ~/VM\ Shared/shrek/
 echo " Done"
 touch ~/VM\ Shared/touch.txt
 echo "Updated touch file"
